673e623632733d28b4d5a2ec66c5c51dfb1830f6
i.furdzik <izabela.furdzik@pracuj.pl>
2022-01-11 23:04:44 +0100

    feat(app): update eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -16,27 +16,32 @@ module.exports = {
     'es6': true
   },
   rules: {
-    'import/no-extraneous-dependencies': 0,
-    'no-multiple-empty-lines': ['error', { 'max': 1 }],
-    'comma-dangle': ['error', 'never'],
-    'import/no-useless-path-segments': 0,
-    'import/prefer-default-export': 0,
-    'arrow-parens': ['error', 'always'],
     'react/destructuring-assignment': 0,
     'react/sort-prop-types': [2, {
       'ignoreCase': true,
       'requiredFirst': true,
       'callbacksLast': true
     }],
-    'react/jsx-one-expression-per-line': 0,
-    'jsx-a11y/anchor-has-content': 0,
-    'no-confusing-arrow': 0,
-    'space-before-function-paren': 0,
-    'react/jsx-fragments': [1, 'element'],
-    'react/jsx-props-no-spreading': 0,
-    'react/static-property-placement': [1, 'static public field'],
     'import/extensions': 0,
+    'react/function-component-definition': 0,
+    'react/jsx-fragments': [1, 'element'],
+    // 'react/jsx-props-no-spreading': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/static-property-placement': [1, 'static public field'],
+    'import/no-extraneous-dependencies': 0,
+    'import/no-useless-path-segments': 0,
+    'import/prefer-default-export': 0,
+    'jsx-a11y/anchor-has-content': 0,
+    'arrow-parens': ['error', 'always'],
+    'camelcase': 0,
+    'comma-dangle': ['error', 'never'],
+    'function-paren-newline': 0,
+    'no-confusing-arrow': 0,
     'no-else-return': 0,
-    'camelcase': 0
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'no-restricted-exports': ['error', {
+      'restrictedNamedExports': ['foo', 'bar', 'Baz', 'a', 'b', 'c', 'd']
+    }],
+    'space-before-function-paren': 0
   }
 };

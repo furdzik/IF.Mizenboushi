d252f592e3ed140155ebe242d4cdb281d9e7e614
furdzik <izabela.furdzik@gmail.com>
2023-09-24 14:13:00 +0200

    fix(app): change in rules after update

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -28,12 +28,9 @@ module.exports = {
         ]
       }
     ],
-    'block-closing-brace-empty-line-before': 'never',
-    'color-hex-case': 'lower',
     'color-hex-length': 'long',
     'declaration-empty-line-before': null,
     'function-url-quotes': 'always',
-    'indentation': 2,
     'max-nesting-depth': [
       3,
       {
@@ -48,12 +45,7 @@ module.exports = {
         ]
       }
     ],
-    'media-feature-range-operator-space-before': 'always',
-    'media-feature-range-operator-space-after': 'always',
-    'media-feature-parentheses-space-inside': 'never',
     'media-feature-name-no-vendor-prefix': true,
-    'media-feature-colon-space-before': 'never',
-    'media-feature-colon-space-after': 'always',
     'order/properties-order': [
       {
         'properties': [
@@ -417,19 +409,16 @@ module.exports = {
       }
     ],
     'plugin/declaration-block-no-ignored-properties': true,
-    'selector-list-comma-newline-after': 'always-multi-line',
     'selector-no-qualifying-type': true,
     'selector-no-vendor-prefix': true,
     'selector-pseudo-element-colon-notation': 'double',
     'selector-type-no-unknown': [ true, { 'ignore': ['custom-elements', 'default-namespace'] }],
-    'string-quotes': 'single',
     'no-descending-specificity': null,
     'value-keyword-case': null,
     'selector-type-case': null,
     'alpha-value-notation': null,
     'selector-class-pattern': null,
     'font-family-name-quotes': 'always-unless-keyword',
-    'number-leading-zero': 'never',
     'declaration-colon-newline-after': null,
 
     // Styled components

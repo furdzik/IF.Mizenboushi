f3dd2e05bbaeef4921ad1986cf8be92f8f97eb76
Izabela Furdzik <izabela.furdzik@grandparade.co.uk>
2025-02-15 09:20:24 +0100

    fix(tslint): add tslint package

---
+++ packages/tslint-config/README.md
@@ -0,0 +1,37 @@
+# eslint-config-kamisama
+
+'eslint' configuration by style guide [IF.Kamisama](https://github.com/furdzik/IF.Kamisama).  
+Contains different framework and library lint configurations and also a global lint which can be used by any JavaScript project.
+
+## Getting started
+
+### Installation
+
+**Peer dependencies**
+- `eslint`
+- `@babel/core`
+- `@babel/eslint-parser`
+- `babel-plugin-module-resolver`
+- `eslint-import-resolver-babel-module`
+
+```
+  npm install --save-dev eslint-config-kamisama
+```
+
+### Usage
+
+**Config `.eslintrc`**
+
+```json
+  {
+    "parser": "@babel/eslint-parser",
+      "extends": [
+        "eslint-config-kamisama"
+      ],
+      "settings": {
+      "import/resolver": {
+        "babel-module": {}
+      }
+    }
+  }
+```
---
+++ packages/tslint-config/index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = {
+  "extends": [
+    "react-app",
+    "react-app/jest",
+    "plugin:jest/recommended",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "rules": {},
+  "parser": "@babel/eslint-parser",
+  "overrides": [
+    {
+      "files": [
+        "**/*.ts?(x)"
+      ],
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        "ecmaFeatures": {
+          "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+      },
+      "rules": {
+        "arrow-parens": [
+          "error",
+          "always"
+        ],
+        "indent": ["error", 2, {
+          "SwitchCase": 1,
+          "VariableDeclarator": 1,
+          "MemberExpression": 1,
+          "ArrayExpression": 1,
+          "ObjectExpression": 1
+        }],
+        "function-call-argument-newline": [
+          "error",
+          "consistent"
+        ],
+        "max-len": ["error", { "code": 200 }],
+        "no-multiple-empty-lines": [
+          "error",
+          {
+            "max": 1
+          }
+        ],
+        "@typescript-eslint/comma-dangle": ["error", "never"],
+        "@typescript-eslint/naming-convention": [
+          0,
+          {
+            "selector": "property",
+            "format": [
+              "camelCase",
+              "PascalCase",
+              "UPPER_CASE"
+            ],
+            "leadingUnderscore": "allow",
+            "trailingUnderscore": "allow",
+            "filter": { "regex": "^[0-9]", "match": false }
+          }
+        ],
+        "@typescript-eslint/no-empty-function": [1, { "allow": ["arrowFunctions"] }],
+        "@typescript-eslint/space-before-function-paren": ["error", "always"]
+      }
+    }
+  ]
+};
